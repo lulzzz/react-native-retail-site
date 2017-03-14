28e90a0189f462be7b15f5b64ad0a777d06820e6
Michael Walsh <michael.c.walsh1@gmail.com>
2017-03-14 17:25:28 -0400

    moved main layout to MainLayout component

--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base';
+import MainLayout from './src/components/layouts/MainLayout';
+import { AppRegistry } from 'react-native';
 import * as firebase from 'firebase';
 
 
 class RetailSite extends Component {
   render() {
     return (
-      <Container>
-        <Header>
-            <Left>
-                <Button transparent>
-                </Button>
-            </Left>
-            <Body>
-                <Title>RetailSite</Title>
-            </Body>
-            <Right />
-        </Header>
-
-        <Content>
-        </Content>
-
-        <Footer>
-            <FooterTab>
-                <Button full>
-                    <Text>Footer</Text>
-                </Button>
-            </FooterTab>
-        </Footer>
-      </Container>
+      <MainLayout />
     );
   }
 }
