42b467e48a9facda1a6f4137a4598917add793d5
Michael Walsh <michael.c.walsh1@gmail.com>
2017-03-21 14:57:40 -0400

    converted HomeIndex and HomeIteration to stateful components

--- src/components/pages/HomeIndex.js
+++ src/components/pages/HomeIndex.js
@@ -1,29 +1,36 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeIteration from './HomeIteration';
 import styles from '../../../styles.js';
 
-const HomeIndex = props => {
-  let allProducts = props.allProducts;
+class HomeIndex extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
 
-  return (
-    <View>
-      <Text style={styles.homeHeader}>New Arrivals</Text>
-      <View style={styles.homeIndexContainer}>
-          {allProducts.map(function(object) {
-            return (
-              <HomeIteration
-                key={object.id}
-                id={object.id}
-                name={object.name}
-                cost={object.cost}
-                image={object.image}
-              />
-            )
-          })}
+
+  render () {
+    return (
+      <View>
+        <Text style={styles.homeHeader}>New Arrivals</Text>
+        <View style={styles.homeIndexContainer}>
+            {this.props.allProducts.map(function(props) {
+              return (
+                <HomeIteration
+                  key={props.id}
+                  id={props.id}
+                  name={props.name}
+                  cost={props.cost}
+                  image={props.image}
+                />
+              )
+            })}
+        </View>
       </View>
-    </View>
-  )
+    )
+  }
 }
 
 export default HomeIndex;
--- src/components/pages/HomeIteration.js
+++ src/components/pages/HomeIteration.js
@@ -1,19 +1,26 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View, Image, Text, Navigator, TouchableHighlight } from 'react-native';
 import styles from '../../../styles.js';
 
-const HomeIteration = props => {
+class HomeIteration extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
 
-  return (
-    <TouchableHighlight onPress={this._onPressButton}>
-      <View style={styles.liContainer}>
-          <Image source={{uri: props.image}} style={styles.liImage}>
-          <Text style={styles.liTextName}>{props.name}</Text>
-          <Text style={styles.liTextCost}>${props.cost}</Text>
-          </Image>
-      </View>
-    </TouchableHighlight>
-  )
+  render() {
+    return (
+      <TouchableHighlight onPress={this._onPressButton}>
+        <View style={styles.liContainer}>
+            <Image source={{uri: this.props.image}} style={styles.liImage}>
+            <Text style={styles.liTextName}>{this.props.name}</Text>
+            <Text style={styles.liTextCost}>${this.props.cost}</Text>
+            </Image>
+        </View>
+      </TouchableHighlight>
+    )
+  }
 }
 
 export default HomeIteration;
