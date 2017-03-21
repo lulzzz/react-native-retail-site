fb96c3555db6a26639d9b93188e16cbe85ae4b70
Michael Walsh <michael.c.walsh1@gmail.com>
2017-03-21 16:47:13 -0400

    added ShowPage component

---
+++ src/components/pages/ShowPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { RNRSData } from '../../config/FirebaseConstants';
+import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
+import styles from '../../../styles.js';
+
+class ShowPage extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default ShowPage;
